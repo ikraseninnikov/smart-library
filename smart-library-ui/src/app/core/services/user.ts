2283767aa007f35e52c8fd654e7bc9e4fa640abc
Ivan Krasheninnikov <geff@inbox.lv>
2025-09-11 19:49:17 +0300

    Created material users list on frontend

--- smart-library-ui/src/app/core/services/user.ts
+++ smart-library-ui/src/app/core/services/user.ts
@@ -4,5 +4,5 @@ export interface User {
   id: number;
   name: string;
   email: string;
-  createdAt?: string;
+  createdAt: string;
 }
--- smart-library-ui/src/app/features/users/user-list/user-list.ts
+++ smart-library-ui/src/app/features/users/user-list/user-list.ts
@@ -1,10 +1,19 @@
-import { Component, OnInit, NgZone, ChangeDetectorRef  } from '@angular/core';
+import { Component, OnInit, NgZone, ChangeDetectorRef, ViewChild  } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 import { UserService } from '../../../core/services/user.service';
 import { User } from '../user';
 
@@ -13,26 +22,53 @@ import { User } from '../user';
   templateUrl: './user-list.html',
   styleUrls: ['./user-list.css'],
   standalone: true,
-  imports: [CommonModule, HttpClientModule, RouterModule, MatButtonModule, MatIconModule, MatToolbarModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatGridListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule
+    ],
   providers: [UserService]
 })
 export class UserListComponent implements OnInit {
 
   users: User[] = [];
   errorMessage: string = '';
+  displayedColumns: string[] = ['name', 'email', 'createdAt', 'actions'];
+  dataSource = new MatTableDataSource<User>([]);
 
-  constructor(private userService: UserService, private cd: ChangeDetectorRef) { }
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private cd: ChangeDetectorRef
+    ) { }
 
   ngOnInit(): void {
     console.log('UserListComponent ngOnInit');
     this.loadUsers();
   }
 
+  ngAfterViewInit(): void {
+    if (this.dataSource) {
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+
   loadUsers(): void {
     this.userService.getUsers().subscribe({
       next: (data) => {
-        this.users.length = 0;
-        this.users.push(...data);
+        this.users = data;
+        this.dataSource = new MatTableDataSource(this.users);
         this.cd.detectChanges();
       },
       error: (err) => {
@@ -42,10 +78,23 @@ export class UserListComponent implements OnInit {
     });
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  editUser(user: User): void {
+    this.router.navigate(['/users', 'edit', user.id]).catch((err: any) => {
+      console.error('Error routing edit user', err);
+    });
+  }
+
   deleteUser(id: number): void {
     this.userService.deleteUser(id).subscribe({
       next: () => {
         this.users = this.users.filter(user => user.id !== id);
+        this.dataSource.data = this.users;
+        this.cd.detectChanges();
       },
       error: (err: any) => console.error('Error on user delete', err)
     });
